fe9eea99e453233ac047d00e40e679318adf6a15
sceiler <sceiler14@gmail.com>
2018-06-06 22:23:31 +0200

    Add decoder test

--- YYang.DocumentManagementSystem.Core.UnitTest/Base64ConverterTests.cs
+++ YYang.DocumentManagementSystem.Core.UnitTest/Base64ConverterTests.cs
@@ -10,11 +10,11 @@ using System.Threading.Tasks;
 
 namespace YYang.DocumentManagementSystem.Core.UnitTest
 {
-    [TestFixture()]
+    [TestFixture]
     public class Base64ConverterTests
     {
-        [Test()]
-        public void EncodeTest()
+        [Test]
+        public void EncodeToBase64Test()
         {
             Byte[] bytes = File.ReadAllBytes(Path.Combine(TestContext.CurrentContext.TestDirectory, @"README.md"));
 
@@ -22,6 +22,18 @@ namespace YYang.DocumentManagementSystem.Core.UnitTest
             converter.Encode();
             
             Assert.IsTrue(Regex.IsMatch(((Base64Converter)converter).base64String, @"^[a-zA-Z0-9\+/]*={0,2}$"));
+        }
+
+        [Test]
+        public void DecodeFromBase64Test()
+        {
+            var base64String = "IyBEb2N1bWVudCBNYW5hZ2VtZW50IFN5c3RlbQ0KDQpUaGlzIGxpdHRsZSBhcHBsaWNhdGlvbiBpcyBzdXBwb3NlZCB0byB3YXRjaCBhIGRpcmVjdG9yeSB3aGVyZSBzY2FubmVkIGRvY3VtZW50cyAoanBnLCBwbmcgYW5kIHBkZikgYXJlIHNhdmVkIHRvLg0KDQpVcG9uIGRldGVjdGlvbiBvZiBzdWNoIGFuIGFjdGlvbiB0aGUgZG9jdW1lbnQncyBtZXRhIGRhdGEgYXJlIHJlYWQgYW5kIHNhdmVkIHRvIGEgZGF0YWJhc2UuDQoNCkVhY2ggZG9jdW1lbnQgaXMgZmxhZ2dlZCBbSU5JVElBTCwgRU5SSUNITUVOVCwgU0FWRUQsIEFSQ0hJVkVEXS4NCg0KIyMgRG9jdW1lbnQgc3RhZ2UNCg0KVGhpcyBzZWN0aW9uIGRlc2NyaWJlZCB0aGUgZGlmZmVyZW50IGRvY3VtZW50IHN0YWdlcy4gQSBkb2N1bWVudCBzdGFnZSB3b3JrZmxvdyBpcyB1c3VhbGx5IElOSVRJQUwgLT4gRU5SSUNITUVOVCAtPiBTQVZFRCAtPiBBUkNISVZFRC4NCg0KIyMjIElOSVRJQUwNCiMjIyBFTlJJQ0hNRU5UDQojIyMgU0FWRUQNCiMjIyBBUkNISVZFRA0KDQojIyBEb2N1bWVudCBzdGF0dXMNCg0KVGhpcyBzZWN0aW9uIGRlc2NyaWJlZCB0aGUgZGlmZmVyZW50IGRvY3VtZW50IHN0YXR1cy4gQSBkb2N1bWVudCBzdGF0dXMgaXMgb25lIG9mIFtPUEVOLCBJTi1QUk9HUkVTUywgUkVWSUVXLCBSRVNPTFZFLCBDTE9TRV0uDQoNCiMjIyBPUEVODQojIyMgSU4tUFJPR1JFU1MNCiMjIyBSRVZJRVcNCiMjIyBSRVNPTFZFDQojIyMgQ0xPU0UNCg==";
+            Byte[] bytes = File.ReadAllBytes(Path.Combine(TestContext.CurrentContext.TestDirectory, @"README.md"));
+
+            IConverter converter = new Base64Converter(base64String);
+            converter.Decode();
+
+            Assert.AreEqual(bytes, ((Base64Converter)converter).bytes);
         }
     }
 }
